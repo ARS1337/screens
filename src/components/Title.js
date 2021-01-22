0bc89526218a36d868fa3ad9d47291f8081356fb
Abhay Singh <abhaysingh17498@gmail.com>
2021-01-22 22:23:35 +0530

    title-responsive but searchbar remaining

--- src/components/Title.js
+++ src/components/Title.js
@@ -6,21 +6,23 @@ function Title() {
   return (
     <>
       {/* <div class="title"> */}
-      <img src="images jpg/title/pukkao_final-2.png" class="logo" width="72" height="69"/>
+      <div class="logo">
+      <img src="images jpg/title/pukkao_final-2.png" />
+      </div>
       <div class="company-name" >
-      <img src="images jpg/title/logo white.png" width="250" height="34"/>
+      <img src="images jpg/title/logo white.png" />
       </div>
       <div class="search-box">
-          <input type="text"/>
+          <input type="text" class="search"/>
           </div>
       <div class="help">
         <img src="images jpg/title/Warning.png"  />
-        <h2>Help</h2>
+        <p>Help</p>
       </div>
 
       <div class="user-login">
         <img src="images jpg/title/Profile.png" alt="" />
-        <h4>Siddhu</h4>
+        <p>Siddhu</p>
       </div>
 
       {/* </div> */}
