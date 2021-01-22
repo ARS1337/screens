158d954739a8f9a570435a973f94f3d307b24285
Abhay Singh <abhaysingh17498@gmail.com>
2021-01-22 20:26:11 +0530

    guk

--- src/App.js
+++ src/App.js
@@ -9,72 +9,59 @@ import Playground from "./components/Playground";
 import Reviews from "./components/Reviews";
 import Recommended from "./components/Recommended";
 import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link,
-    useRouteMatch,
-    useParams,
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams,
 } from "react-router-dom";
 
 function App() {
-    return ( <
-        Router >
+  return (
+    <Router>
 
-        <
-        div className = "App" >
-        <
-        div class = "main" > { /* <div class="main-container"> */ } <
-        div class = "main-title" > { /* <Title/> */ } { /* </div> */ } <
-        div class = "main-header" >
-        <
-        Header / >
-        <
-        /div> <
-        div class = "main-navigation" >
-        <
-        Navigation / >
-        <
-        /div> <
-        Switch > { /* <Route path="/Recommended"> */ } { /* using Recommended as Home for now*/ } <
-        Route exact path = "/" >
-        <
-        div class = "main-content" >
-        <
-        Recommended / >
-        <
-        /div> <
-        /Route> <
-        Route exact path = "/Recommended" >
-        <
-        div class = "main-content" >
-        <
-        Recommended / >
-        <
-        /div> <
-        /Route> <
-        Route path = "/Reviews" >
-        <
-        div class = "main-content" >
-        <
-        Reviews / >
-        <
-        /div> <
-        /Route> <
-        Route path = "/Videos" >
-        <
-        div class = "main-content" >
-        <
-        Videos / >
-        <
-        /div> <
-        /Route> <
-        /Switch> <
-        /div> <
-        /div> <
-        /div> <
-        /Router>
-    );
+    <div className="App">
+      <div class="main">
+        {/* <div class="main-container"> */}
+          <div class="main-title">
+            <Title/>
+          </div>
+          <div class="main-header">
+            <Header />
+          </div>
+          <div class="main-navigation">
+            <Navigation />
+          </div>
+          <Switch>
+            {/* <Route path="/Recommended"> */}
+            {/* using Recommended as Home for now*/}
+            <Route exact path="/"> 
+              <div class="main-content">
+                <Recommended />
+              </div>
+            </Route>
+            <Route exact path="/Recommended"> 
+              <div class="main-content">
+                <Recommended />
+              </div>
+            </Route>
+            <Route path="/Reviews">
+              <div class="main-content">
+                <Reviews />
+              </div>
+            </Route>
+            <Route path="/Videos">
+              <div class="main-content">
+                <Videos />
+              </div>
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </div>
+    </Router>
+  );
 }
 
 export default App;
