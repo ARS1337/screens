44af87b9335b0e39e9cef458f62f8cbfc20ee5a7
Abhay Singh <abhaysingh17498@gmail.com>
2021-01-25 14:00:13 +0530

    title-searchbar in progress

--- src/components/Title.js
+++ src/components/Title.js
@@ -7,16 +7,38 @@ function Title() {
     <>
       {/* <div class="title"> */}
       <div class="logo">
-      <img src="images jpg/title/pukkao_final-2.png" />
+        <img src="images jpg/title/pukkao_final-2.png" />
       </div>
-      <div class="company-name" >
-      <img src="images jpg/title/logo white.png" />
+      <div class="company-name">
+        <img src="images jpg/title/logo white.png" />
       </div>
       <div class="search-box">
-          <input type="text" class="search"/>
-          </div>
+        <div class="search-box-left">
+          <img src="images jpg/title/location-marker.png" />
+          <select id="cars" name="cars" class="location-list">
+            <option value="volvo" selected>
+              Volvo
+            </option>
+            <option value="saab">Saab</option>
+            <option value="fiat">Fiat</option>
+            <option value="audi">Audi</option>
+          </select>
+        </div>
+        <div class="vl1"/>
+        <div class="search-box-center">
+          <input
+            type="text"
+            class="search"
+            value="Search for dishes, Restaurants"
+          />
+        </div>
+        <div class="vl2"/>
+        <div class="search-box-right">
+          <img src="images jpg/title/Icon search.png" />
+        </div>
+      </div>
       <div class="help">
-        <img src="images jpg/title/Warning.png"  />
+        <img src="images jpg/title/Warning.png" />
         <p>Help</p>
       </div>
 
